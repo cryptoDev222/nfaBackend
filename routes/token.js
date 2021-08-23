a740ee6090b62c0ad91c3a1e4cabd96b2c11d7ea
fullstackdev215 <genius.developer.63@gmail.com>
2021-08-23 19:03:31 +0200

    test

--- routes/token.js
+++ routes/token.js
@@ -40,7 +40,7 @@ router.get("/stakedTokens", async function (req, res) {
   let datas = [];
   if (ids !== "")
     datas = await query(
-      "SELECT  Distinct name, gender, token_id, class, initiate_flag, img_url, traits, account_id from tb_tokens WHERE `token_id` IN (" +
+      "SELECT  Distinct name, gender, token_id, class, initiate_flag, img_url, traits, account_id, baby_count from tb_tokens WHERE `token_id` IN (" +
         ids +
         ")",
       []
